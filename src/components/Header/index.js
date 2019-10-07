4d8c2aa34aaaad6b9c19e3adcecc2745c5c8d12b
MarioCesarJr <mcsjuniorsouza@gmail.com>
2019-10-07 13:15:56 -0300

    header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+    return (
+        <Container>
+            <img src={logo} alt="Agrotis" />
+            <span>AGROTIS</span>
+        </Container>
+    );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    background: #fff;
+    padding: 10px 30px;
+    border-bottom: 1px solid rgb(0, 0, 0);
+
+    display: flex;
+    align-items: center;
+
+    img {
+        width: 32px;
+        height: 32px;
+    }
+
+    span {
+        font-weight: bold;
+        margin-left: 5px;
+        color: #3b3839;
+        font-size: 20px;
+    }
+`;
